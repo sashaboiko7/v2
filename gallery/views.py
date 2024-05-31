29a5109816baf8b541d84ed1d33e70d40ea8eb78
Oleksandra <oleksandraboiko777@gmail.com>
2024-05-31 10:55:49 +0300

    Add logic for image_detail

--- gallery/views.py
+++ gallery/views.py
@@ -6,6 +6,6 @@ def gallery_view(request):
     categories = Category.objects.all()
     return render(request, 'gallery.html', { 'categories': categories })
 
-def image_detail(request):
-    # Your code here
-    return render(request, 'image_detail.html')
+def image_detail(request, pk):
+    image = get_object_or_404(Image, pk=pk)
+    return render(request, 'image_detail.html', {'image': image})
