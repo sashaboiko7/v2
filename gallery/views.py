2fbcc83431289e89527e2265b17f08cf34147646
Oleksandra <oleksandraboiko777@gmail.com>
2024-05-31 10:54:06 +0300

    Logic for gallery_view

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-
 from django.shortcuts import render
+from .models import Image, Category
 
 def gallery_view(request):
+    categories = Category.objects.all()
+    return render(request, 'gallery.html', { 'categories': categories })
+
+def image_detail(request):
     # Your code here
-    return render(request, 'gallery_view.html')
+    return render(request, 'image_detail.html')
